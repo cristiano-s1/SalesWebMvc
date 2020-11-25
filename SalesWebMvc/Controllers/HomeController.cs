8fb36685d2d2137ed24a79803153f95fe1e50b93
cristiano campos <cristiano.souzac@hotmail.com.br>
2020-11-25 20:21:39 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -10,30 +10,36 @@ namespace SalesWebMvc.Controllers
 {
     public class HomeController : Controller
     {
+        //View Index
         public IActionResult Index()
         {
             return View();
         }
 
+        //View About
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales control system Asp. Net Core MVC of the C # course at Udemy";
+            ViewData["Aluno"] = "Cristiano Campos de Souza";
 
             return View();
         }
 
+        //View Contact
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "Contact page.";
 
             return View();
         }
 
+        //View Privacy
         public IActionResult Privacy()
         {
             return View();
         }
 
+        ////View Error
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
